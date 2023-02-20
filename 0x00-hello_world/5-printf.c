03c64e3b958e2d0f4cb44d65c4beaf59da823806
Dolarpoh <dolapo.durojaiye@outlook.com>
2023-02-20 12:44:24 -0800

    Hello Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the primary function
+ *
+ * Return: 0 at full success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
