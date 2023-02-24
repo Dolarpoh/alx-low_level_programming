3086ccbdbd97ab4885b603665e05e07574d9bb30
Dolarpoh <dolapo.durojaiye@outlook.com>
2023-02-24 10:51:26 -0800

    Collaboration

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - this will checheck for uppercase characters
+ * @c: this is the character to be checked
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int_isupper(int c)
+
+{
+	return(c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Newest ReadMe for Nested Loops
